cca82fe146ae10167497fb9ca1255077ef050133
RandomMinion <160042226+BerthamN@users.noreply.github.com>
2024-11-17 00:38:04 +0700

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">                
+                <div class="p-6 text-gray-900">
+                    <p>Selamat Datang
+                        <b> 
+                            {{Auth::user()->name}} 
+                        </b>
+                        di website kami
+                    </p> 
+                    <p><br>Ini baru testing</p>
+                    <center>
+                        <h6>
+                            Cebelapa imut ci aq 🥺👉👈
+                        </h6>
+                    </center>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+</x-app-layout>
